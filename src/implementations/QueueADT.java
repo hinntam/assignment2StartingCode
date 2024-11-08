f1a22fd85732bafb0f4b5185fd06100135f15ee0
John <ngoctam.nguyen@edu.sait.ca>
2024-11-07 21:05:50 -0700

    Updated pos and pre from David
    Updated pos and pre from David

--- src/implementations/QueueADT.java
+++ src/implementations/QueueADT.java
@@ -10,7 +10,11 @@ import java.io.Serializable;
 public interface QueueADT <T>{
     /**
      * Adds an element to the rear of the queue.
-     *
+     * 
+     * @precondition element must not be null
+     * @postcondition element is added to the end of the queue, the size of 
+     * the queue is increased by 1, and the queue has new element at the end
+     * if operation is successful 
      * @param element The element to be added.
      * @return true if the element was successfully added.
      * @throws NullPointerException if the specified element is null.
@@ -20,6 +24,10 @@ public interface QueueADT <T>{
     /**
      * Removes and returns the element at the front of the queue.
      *
+     * @precondition queue must not be empty
+     * @postcondition element is removed from the front of the queue and returned.
+     * The size of the queue is decreased by 1, and the next element is now at
+     * the front of the queue if the operation is successful.
      * @return The element at the front of the queue.
      * @throws java.util.NoSuchElementException if the queue is empty.
      */
@@ -28,6 +36,9 @@ public interface QueueADT <T>{
     /**
      * Returns the element at the front of the queue without removing it.
      *
+     * @precondition queue must not be empty
+     * @post condition element at the front of the queue is returned.
+     * Size of queue remains the same.
      * @return The element at the front of the queue.
      * @throws java.util.NoSuchElementException if the queue is empty.
      */
@@ -36,6 +47,7 @@ public interface QueueADT <T>{
     /**
      * Checks if the queue is empty.
      *
+     * @postcondition returns true if queue contains no elements, otherwise returns false
      * @return true if the queue contains no elements.
      */
     public boolean isEmpty();
@@ -43,12 +55,16 @@ public interface QueueADT <T>{
     /**
      * Returns the number of elements in the queue.
      *
+     * @postcondition returns the size of the queue 
      * @return The number of elements in the queue.
      */
     public int size();
 
     /**
      * Removes all elements from the queue.
+     * 
+     * @postcondition if not null, all element are removed from the queue.
+     * The queue is empty
      */
     public void clear();
 
--- src/implementations/StackADT.java
+++ src/implementations/StackADT.java
@@ -6,9 +6,13 @@ import utilities.Iterator;
  * @author Emily
  */
 public interface StackADT<T> {
-    /**
+     /**
      * Pushes an element onto the top of the stack.
      *
+     * @precondition element must not be null
+     * @postcondition element is added at the top of the stack.
+     * The size of the stack increases  by 1 and element is put 
+     * over the previous top element
      * @param element The element to be pushed onto the stack.
      * @return true if the element was successfully pushed.
      * @throws NullPointerException if the specified element is null.
@@ -18,6 +22,10 @@ public interface StackADT<T> {
     /**
      * Removes and returns the top element from the stack.
      *
+     * @precondition stack cannot be empty
+     * @postcondition element at the top is removed from the stack and returned.
+     * The size of the stack is decreased by 1 and the new next element
+     * is now the top element.
      * @return The top element of the stack.
      * @throws java.util.EmptyStackException if the stack is empty.
      */
@@ -26,6 +34,9 @@ public interface StackADT<T> {
     /**
      * Returns the top element of the stack without removing it.
      *
+     * @precondition stack must not be empty
+     * @postcondition element at the top is of the stack is returned.
+     * The size of the stack remains the same.
      * @return The top element of the stack.
      * @throws java.util.EmptyStackException if the stack is empty.
      */
@@ -34,6 +45,7 @@ public interface StackADT<T> {
     /**
      * Checks if the stack is empty.
      *
+     * @postcondition returns true if stack has no elements, otherwise it returns false
      * @return true if the stack contains no elements.
      */
     public boolean isEmpty();
@@ -41,12 +53,15 @@ public interface StackADT<T> {
     /**
      * Returns the number of elements in the stack.
      *
+     * @postcondition returns size of stack
      * @return The number of elements in the stack.
      */
     public int size();
 
     /**
      * Removes all elements from the stack.
+     * 
+     * @postconditions removes all elements from stack
      */
     public void clear();
 
